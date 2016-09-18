a3e8fd871f126cb8e929d6772924cfc01a562520
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-09-18 10:29:39 -0700

    Added sidebar fixation

--- _site/public/js/courses.js
+++ _site/public/js/courses.js
@@ -1,10 +1,64 @@
+// Dank Sidebar
 $(document).ready(function() {
-  console.log("wow");
+  var sidebarTop = 80;
+  var listTop = $(".course-list").offset().top;
+  var tmp;
+  var scrollTop;
+
+  // Fixes the sidebar at certain height
   $(window).scroll(function() {
-    console.log("wow");
-    if ($(window).scrollTop() > $(".course-list").scrollTop()) {
-      console.log("reeeek");
+    scrollTop = $(window).scrollTop();
+
+    if (scrollTop > listTop - sidebarTop) {
+      tmp = $(".courses-majors").offset().left;
+      $(".courses-majors").css({
+        "position": "fixed",
+        "top": sidebarTop,
+        "left": tmp
+      });
     }
-    $(".course-list").css("color", "red !important");
+    else {
+      unHighlightSidebar();
+      $(".courses-majors").css({
+        "position": "absolute",
+        "top": 0,
+        "left": 15
+      });
+    }
+
+    // Highlights section based on scrolling
+    if (scrollTop < $("#compsci").offset().top - sidebarTop) {
+      unHighlightSidebar();
+      sidebarHighlight("#link-cogsci");
+    }
+    else if (scrollTop >= $("#compsci").offset().top - sidebarTop &&
+        scrollTop < $("#dsgn").offset().top - sidebarTop) {
+      unHighlightSidebar();
+      sidebarHighlight("#link-compsci");
+    }
+    else if (scrollTop >= $("#dsgn").offset().top - sidebarTop &&
+        scrollTop < $("#other").offset().top - sidebarTop) {
+      unHighlightSidebar();
+      sidebarHighlight("#link-dsgn");
+    }
+    else if (scrollTop >= $("#other").offset().top - sidebarTop) {
+      unHighlightSidebar();
+      sidebarHighlight("#link-other");
+    }
   });
+
 });
+
+// Highlight and unhighlight functions
+function unHighlightSidebar() {
+  $(".courses-majors a").css({
+    "font-weight": "400",
+    "text-decoration": "none"
+  });
+}
+function sidebarHighlight(sel) {
+  $(sel).css({
+    "font-weight": "bold",
+    "text-decoration": "underline"
+  });
+}
